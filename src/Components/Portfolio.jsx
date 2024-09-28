b3d9817208ce9ed66cf769110c62d9daa635382f
Bruno Viola <99220989+BrunoViola@users.noreply.github.com>
2024-09-28 14:40:20 +0000

    fix: novas alterações no conteúdo do site

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -37,22 +37,21 @@ const projectList = [
     url: "https://github.com/BrunoViola/jogo-da-vida",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Maze solver using backtracking algorithm",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "This is a project developed in C language as part of the Data Structures I course.",
+    url: "https://github.com/BrunoViola/Labirinto-com-backtracking",
   },
   {
-    title: "My Resume Site",
+    title: "Web and Mobile Development",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "This repository contains projects such as a Web Delivery App and a mobile application developed using React Native.",
+    url: "https://github.com/BrunoViola/lab-prog",
   },
   {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+    title: "Beecrowd",
+    description: "This repository contains solutions to problems from the Beecrowd platform.",
+    url: "https://github.com/BrunoViola/Beecrowd",
   },
 ];
 
